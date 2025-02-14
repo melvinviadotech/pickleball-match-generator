0d8c076112b41c38e8296155ad6a8888cb476eec
melvinviadotech <123508074+melvinviadotech@users.noreply.github.com>
2025-02-13 20:31:28 -0500

    Organized dart files in library Working on Database usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'format.dart';
+import 'features/tournaments/screens/create_tournament.dart';
+import 'widgets/main_drawer.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,17 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  backgroundColor: Color(121212),
+                  foregroundColor: Colors.black))),
       title: 'Robin Tool',
-      home: MyHomePage(title: 'Create a session'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(title: 'Create a session'),
     );
   }
 }
@@ -30,58 +39,29 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-                width: 500.0,
-                height: 60.0,
-                child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RoundRobin()),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(10), // Set the radius here
-                      ),
-                    ),
-                    child: const Column(children: <Widget>[
-                      Text(
-                        'Round Robin',
-                        style: TextStyle(fontSize: 24.0),
-                      ),
-                      Text('Generate matchups, add scores and see who wins')
-                    ]))),
-            const SizedBox(height: 10),
-            SizedBox(
-                width: 500.0,
-                height: 50.0,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(10), // Set the radius here
-                    ),
-                  ),
-                  child: const Text(
-                    'Tournament Bracket',
-                    style: TextStyle(fontSize: 24.0),
-                  ),
-                )),
-          ],
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
         ),
-      ),
-    );
+        drawer: MainDrawer(),
+        body: TournamentsPage());
+  }
+}
+
+class PlayerPage extends StatelessWidget {
+  const PlayerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text('Player List'),
+        ),
+        drawer: MainDrawer(),
+        body: Container(
+          margin: EdgeInsets.all(15.0),
+          child: Center(child: const Text('Player List')),
+        ));
   }
 }
